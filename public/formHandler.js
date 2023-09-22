b977928445e5935af5ef13f591f800d09921bc9b
Mohamed Sharief <hello@moe-sharief.dev>
2023-09-22 17:43:12 +0300

    added logs

--- public/formHandler.js
+++ public/formHandler.js
@@ -28,16 +28,23 @@ document.addEventListener('DOMContentLoaded', function() {
     .then(data => {
       console.log('Success from aggregateApiCalls:', data);
 
+        // New Debugging Line: Check the integrity of returned data
+        console.log("Data to be populated:", data.filteredData);
+
       // Clear existing cards if any
       for (let i = 1; i <= data.filteredData.length; i++) {
         const cardContainer = document.getElementById(`resultscard${i}`);
         if (cardContainer) {
           cardContainer.innerHTML = '';
+        } else {
+          console.log(`resultscard${i} not found`);
         }
       }
       
       // Iterate through the filtered data and populate the HTML cards
       data.filteredData.forEach((item, index) => {
+          // New Debugging Lines: Log each item to be populated
+          console.log(`Populating card ${index + 1} with:`, item);
         document.getElementById(`businessinfotitle${index + 1}`).innerText = item.name;
         document.getElementById(`businesswebaddress${index + 1}`).innerText = `🌐 Website; ${item.website}`;
         document.getElementById(`businessphoneaddress${index + 1}`).innerText = `📞 Sales Phone Number; ${item.phoneNumber}`;
@@ -46,7 +53,13 @@ document.addEventListener('DOMContentLoaded', function() {
     })
     .catch((error) => {
       console.error('Error:', error);
-    });
+
+       // New Error Handling: Show error in the first result card
+       const firstCard = document.getElementById('resultscard1');
+       if (firstCard) {
+         firstCard.innerHTML = '<p>An error occurred while fetching data.</p>';
+       }
+     });
 
     return false;  // Prevent the default form submission behavior
   });
