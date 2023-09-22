e7144d6d8e0033d8382c0b11b9382dc3b7a50cd8
Mohamed Sharief <hello@moe-sharief.dev>
2023-09-22 14:53:31 +0300

    formHandler Fix

--- public/formHandler.js
+++ public/formHandler.js
@@ -1,15 +1,15 @@
 // Add this script at the end of your HTML or in a separate JS file
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Get a reference to the form element
-    const searchForm = document.getElementById('searchForm');
-    
-    // Add an event listener for form submission
-    searchForm.addEventListener('submit', function(event) {
-        console.log("Form submitted");  // Debugging line
+  // Get a reference to the form element
+  const searchForm = document.getElementById('searchForm');
+  
+  // Add an event listener for form submission
+  searchForm.addEventListener('submit', function(event) {
+      console.log("Form submitted");  // Debugging line
       event.preventDefault(); // Prevent the default form submission behavior
-      return false;  // Add this line
-      
+
+      // Moved these lines above the 'return false;' line
       // Get the search query from the form
       const searchQuery = document.getElementById('searchQuery').value;
       
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function() {
       const data = {
         searchQuery: searchQuery
       };
-  
+
       // Make a POST request to the Firebase Cloud Function
       console.log("About to make fetch call");  // Debugging line
       fetch('https://us-central1-prospexa-production.cloudfunctions.net/handleSearch', {
@@ -34,6 +34,7 @@ document.addEventListener('DOMContentLoaded', function() {
       .catch((error) => {
         console.error('Error:', error);
       });
-    });
+
+      return false;  // Moved this line to the end, if needed
   });
-  
+});
